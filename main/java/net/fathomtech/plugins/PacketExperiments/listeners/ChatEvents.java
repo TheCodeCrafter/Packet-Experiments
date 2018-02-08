0c3a6ddca0aa7f5710c340938aa10797cba78950
Noah <noahcoetsee@gmail.com>
2018-02-08 12:18:06 -0500

    Create ChatEvents.java

---
+++ main/java/net/fathomtech/plugins/PacketExperiments/listeners/ChatEvents.java
@@ -0,0 +1,8 @@
+package net.fathomtech.plugins.PacketExperiments.listeners;
+
+public class ChatEvents implements Listener {
+  @EventHandler
+  public void CensorChat(AsyncPlayerChatEvent event) {
+    
+  }
+}
