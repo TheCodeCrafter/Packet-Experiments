7e3ecaa6ea9838c075a11c8ba500791decfce276
Noah <noahcoetsee@gmail.com>
2018-02-08 12:20:01 -0500

    Create PacketExperimentsCommand.java

---
+++ main/java/net/fathomtech/plugins/PacketExperiments/commands/PacketExperimentsCommand.java
@@ -0,0 +1,8 @@
+package net.fathomtech.plugins.PacketExperiments.commands;
+
+public class PacketExperiementsCommand implements CommandExecutor {
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    
+  }
+}
