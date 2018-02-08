3c28718108cadb9baa9eb51a7eaee4c566a5db80
Noah <noahcoetsee@gmail.com>
2018-02-08 12:11:20 -0500

    Create PacketExperimentsPlugin.java

---
+++ main/java/net/fathomtech/plugins/PacketExperiments/PacketExperimentsPlugin.java
@@ -0,0 +1,15 @@
+package net.fathomtech.plugins.PacketExperiments;
+
+public class PacketExperimentsPlugin extends JavaPlugin {
+  
+  @Override
+  public void onEnable() {
+  
+  }
+  
+  @Override
+  public void onDisable() {
+  
+  }
+  
+}
